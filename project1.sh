6d2c93ca6b26efcfde8e8cd3bac063ea29414ff9
gauthambejoy <gauthambejoyprof@gmail.com>
2025-12-02 06:57:48 +0530

    Update

--- project1.sh
+++ project1.sh
@@ -1,37 +1,98 @@
 #! /bin/bash
 
-echo "NOTES CLI"
-echo "1. Create a new note"
-echo "2. Search for a note"
-echo -n "Enter your choice:(1/2):"
-read choice
+while true
+do
+    echo "NOTES CLI"
+    echo "1. Create a new note"
+    echo "2. Search for a note"
+    echo "3. Edit note"
+    echo "4. Delete note"
+    echo "5. exit"
+    echo -n "Enter your choice:"
+    read choice
 
-create_note() {
-    notes_dir="notes"
+    create_note() {
+        notes_dir="notes"
 
-    if [ ! -d "$notes_dir" ]
+        if [ ! -d "$notes_dir" ]
+        then
+            mkdir "$notes_dir"
+        fi
+
+        last_file=$(ls "$notes_dir"/note*.txt 2>/dev/null | sort -V | tail -n 1)
+        
+
+        if [ -z "$last_file" ]
+        then
+            next_num=1
+        else
+            base_name=${last_file##*/}
+            base_name=${base_name%.*}
+            num=${base_name#note}
+            next_num=$(( num + 1 ))
+        fi
+
+        new_file="$notes_dir/note${next_num}.txt"
+        nano "$new_file"
+    }
+
+    search_note() {
+        echo -n "Enter the keyword to search:"
+        read keyword
+
+        grep -ni "$keyword" notes/note*.txt 2>/dev/null
+
+        if [ $? -ne 0 ]
+        then
+            echo "No notes found with the keyword:$keyword"
+        fi
+    }
+
+    edit_note() {
+        echo -n "Enter the note number:"
+        read number
+
+        filename="notes/note${number}.txt"
+
+        if [ -f "$filename" ]
+        then
+            nano "$filename"
+        else
+            echo "File does not exist"
+        fi
+    }
+
+    delete_note() {
+        echo -n "Enter the note number:"
+        read number
+
+        filename="notes/note${number}.txt"
+
+        if [ -f "$filename" ]
+        then
+            rm "$filename"
+        else
+            echo "File does not exist"
+        fi
+    }
+
+
+    if [ "$choice" -eq 1 ];
     then
-        mkdir "$notes_dir"
-    fi
-
-    last_file=$(ls "$notes_dir"/note*.txt 2>/dev/null | sort -V | tail -n 1)
-    
-
-    if [ -z "$last_file" ]
+        create_note
+    elif [ "$choice" -eq 2 ];
+    then    
+        search_note
+    elif [ "$choice" -eq 3 ];
     then
-        next_num=1
+        edit_note
+    elif [ "$choice" -eq 4 ];
+    then
+        delete_note
+    elif [ "$choice" -eq 5 ];
+    then
+        exit 0
     else
-        base_name=${last_file##*/}
-        base_name=${base_name%.*}
-        num=${base_name#note}
-        next_num=$(( num + 1 ))
+        echo "Wrong Choice"
     fi
-
-    new_file="$notes_dir/note${next_num}.txt"
-    nano "$new_file"
-}
-
-if [ "$choice" -eq 1 ];
-then
-    create_note
-fi
+done

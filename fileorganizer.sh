9399b943786a29518ee31d33ea7b565c48258b6a
gauthambejoy <gauthambejoyprof@gmail.com>
2025-12-16 22:00:20 +0530

    Update

--- fileorganizer.sh
+++ fileorganizer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#part 1 identifying file tyes
 dir=$(pwd)
 echo "Directory:$dir"
 echo "File types in the directory:"
@@ -26,4 +28,39 @@ do
             fi
         fi
     fi
-done 
+done
+for extension in "${ext_arr[@]}"
+do
+    echo "$extension"
+done
+
+#part 2 creating the filetype directory
+
+echo "Enter the file type to be sorted:"
+read filetype
+filetype="${filetype,,}"   
+found=false
+for i in "${ext_arr[@]}"
+do
+    if [[ "$filetype" == "$i" ]]
+    then
+        found=true
+        break
+    fi
+done
+if [[ $found == true ]]
+then
+    mkdir -p "$filetype"
+else
+    echo "Incorrect file type"
+fi
+
+#part 3 moving the files to the new 
+
+for file in *."$filetype"
+do
+    if [[ -f "$file" ]]
+    then
+        mv "$file" "$filetype"/
+    fi
+done
